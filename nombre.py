1f93e31caddfeb259e1f046f131b16d0fc60198f
Lucas <lucasalassia1@outlook.com>
2024-08-13 19:00:34 -0300

    mejoro funcion decir nombre

--- nombre.py
+++ nombre.py
@@ -1,5 +1,10 @@
 def decirNombre(nombre):
     print(f"Hola {nombre}")
+    print("Felicitaciones por haber terminado el curso de Git y GitHub")
 
-decirNombre("Git")
-decirNombre("GitHub")
+def ingNombre():
+    nombre = input("Ingrese su nombre: ")
+    return nombre
+
+nombre = ingNombre()
+decirNombre(nombre)
